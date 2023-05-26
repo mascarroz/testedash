1a66753699fa0b66a75e655e9df4cde68d4b0209
mascarroz <132553761+mascarroz@users.noreply.github.com>
2023-05-26 22:47:46 +0000

    outra alteração

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,4 +1,4 @@
-import { Box, useStyleConfig } from "@chakra-ui/react";
+import { Box, useStyleConfig } from "@chakra-ui/react"; cebolão
 function Card(props) {
   const { variant, children, ...rest } = props;
   const styles = useStyleConfig("Card", { variant });
