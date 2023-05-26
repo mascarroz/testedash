92cf4f72ea0b73fb5ee42c2588d48e9851f6e03f
mascarroz <132553761+mascarroz@users.noreply.github.com>
2023-05-26 19:39:30 -0300

    mais uma alteração

--- src/components/calendar/MiniCalendar.js
+++ src/components/calendar/MiniCalendar.js
@@ -3,9 +3,9 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "assets/css/MiniCalendar.css";
 import { Text, Icon } from "@chakra-ui/react";
-// Chakra imports
+// Chakra imports é só
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
-// Custom components
+// Custom components tudo
 import Card from "components/card/Card.js";
 
 export default function MiniCalendar(props) {
